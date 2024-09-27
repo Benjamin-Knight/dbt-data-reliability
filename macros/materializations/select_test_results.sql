7a4adf8f1752bb6a0fa4bc319749e8b6de259558
Benjamin <Benjamin.Knight@wvt.nhs.uk>
2024-09-27 14:55:54 +0100

    Update select test results to escape the SQL inside the view and to hash the sql to generate the unique identifier for the view

--- macros/materializations/select_test_results.sql
+++ macros/materializations/select_test_results.sql
@@ -12,12 +12,13 @@
 
 {%- macro sqlserver__select_test_results(sql, sample_limit) -%}
     {# We do not support nested CTEs #}
+    {% set test_sql = elementary.escape_special_chars(sql) %}
     {% set test_view %}
-        [{{ target.schema }}.testview_{{ range(1300, 19000) | random }}]
+        [{{ target.schema }}.testview_{{ local_md5(test_sql) }}]
     {% endset %}
-    EXEC('create view {{test_view}} as {{ sql }};');
+    EXEC('create view {{test_view}} as {{ test_sql }};');
 
-    select {% if sample_limit is not none %} top {{ sample_limit }} {% endif %} * from test_view;
+    select {% if sample_limit is not none %} top {{ sample_limit }} {% endif %} * from {{ test_view }};
 
     EXEC('drop view {{test_view}};');
 {%- endmacro -%}
