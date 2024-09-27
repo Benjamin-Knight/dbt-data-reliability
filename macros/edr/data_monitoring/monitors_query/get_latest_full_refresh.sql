d087f3241db52c0d8db6a78fd065980860806d57
Benjamin <Benjamin.Knight@wvt.nhs.uk>
2024-09-27 23:55:47 +0100

    Create adapter specific logic for getting latest full refresh due to usage of limit not being supported on sql server

--- macros/edr/data_monitoring/monitors_query/get_latest_full_refresh.sql
+++ macros/edr/data_monitoring/monitors_query/get_latest_full_refresh.sql
@@ -1,4 +1,8 @@
-{% macro get_latest_full_refresh(model_node) %}
+{%- macro get_latest_full_refresh(model_node) -%}
+    {{ return(adapter.dispatch('get_latest_full_refresh', 'elementary')(model_node)) }}
+{%- endmacro -%}
+
+{% macro default__get_latest_full_refresh(model_node) %}
     {%- set dbt_run_results_relation = elementary.get_elementary_relation('dbt_run_results') %}
     {% set query %}
         select generated_at from {{ dbt_run_results_relation }}
@@ -7,6 +11,18 @@
           full_refresh = true
         order by generated_at desc
         limit 1
+    {% endset %}
+    {% do return(elementary.result_value(query)) %}
+{% endmacro %}
+
+{% macro sqlserver__get_latest_full_refresh(model_node) %}
+    {%- set dbt_run_results_relation = elementary.get_elementary_relation('dbt_run_results') %}
+    {% set query %}
+        select top 1 generated_at from {{ dbt_run_results_relation }}
+        where
+          unique_id = '{{ model_node.unique_id }}' and
+          full_refresh = true
+        order by generated_at desc
     {% endset %}
     {% do return(elementary.result_value(query)) %}
 {% endmacro %}
