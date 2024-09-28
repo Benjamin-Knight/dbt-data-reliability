46f8b353207408105511de1f19da9bcc65026060
Benjamin <Benjamin.Knight@wvt.nhs.uk>
2024-09-28 22:12:24 +0100

    Update boolean handling

--- macros/edr/data_monitoring/schema_changes/get_columns_changes_query.sql
+++ macros/edr/data_monitoring/schema_changes/get_columns_changes_query.sql
@@ -95,7 +95,7 @@
             {{ elementary.null_string() }} as pre_data_type,
             detected_at as detected_at
         from cur
-        where is_new = true
+        where is_new = {{ elementary.print_boolean(TRUE) }}
 
     ),
     {% endif %}
