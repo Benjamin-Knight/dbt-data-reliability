ed468015fbadd6152c30ce03622c58cb70692564
Benjamin <Benjamin.Knight@wvt.nhs.uk>
2024-11-06 21:02:13 +0000

    Update derivation of the test result view for sql server to use the model name rather than the SQL as its more unique

--- macros/materializations/select_test_results.sql
+++ macros/materializations/select_test_results.sql
@@ -13,7 +13,7 @@
 {%- macro sqlserver__select_test_results(sql, sample_limit) -%}
     {# We do not support nested CTEs #}
     {% set test_sql = elementary.escape_special_chars(sql) %}
-    {% set unique_key = local_md5(test_sql) %}
+    {% set unique_key = local_md5(model.name) %}
     {% set test_view %}[{{ target.schema }}.testview_{{ unique_key }}_{{ range(1300, 19000) | random }}]{% endset %}
     EXEC('create view {{ test_view }} as {{ test_sql }};');
 
